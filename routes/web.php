6034cb8f93ce777c1879f794c93d8273b4982890
Md Anisuzzaman <anisuzzzaman2112@gmail.com>
2022-05-17 14:37:31 +0600

    Todays work

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
+    #dd(app());
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
